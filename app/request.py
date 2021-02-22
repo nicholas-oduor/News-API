b80bc31d017992e35a46baa1d27261e91a57912e
nicholas-oduor <oduor5742@gmail.com>
2021-02-22 12:51:22 +0300

    updated get news method

--- app/request.py
+++ app/request.py
@@ -11,4 +11,22 @@ def configure_request(app):
     api_key = app.config['NEWS_API_KEY']
     base_url = app.config['NEWS_API_BASE_URL']
     
+def get_news(source):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_news_url = base_url.format(source,api_key)
+
+    with urllib.request.urlopen(get_news_url) as url:
+        get_news_data = url.read()
+        get_news_response = json.loads(get_news_data)
+
+        news_results = None
+
+        if get_news_response['articles']:
+            news_results_list = get_news_response['articles']
+            news_results = process_results(news_results_list)
+
+
+    return news_results
     
