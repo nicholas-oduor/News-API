edb638b497b0fc820b60cbed4ab4397708b82832
nicholas-oduor <oduor5742@gmail.com>
2021-02-22 12:09:05 +0300

    added class article

---
+++ app/models.py
@@ -0,0 +1,10 @@
+class Article:
+
+    all_articles = []
+
+    def __init__(self,title,image,description,date,article):
+        self.title = title
+        self.image = image
+        self.description = description
+        self.date = date
+        self.article = article
